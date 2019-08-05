032f50b2e8ad8a283a458e6492e4315f186c8291
Khawlamhb <mouhoubi.khawla@gmail.com>
2019-08-05 11:51:36 +0200

    add javadoc

--- core/src/main/java/org/apache/calcite/rel/rules/SortJoinCopyRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/SortJoinCopyRule.java
@@ -38,6 +38,12 @@ import java.util.List;
 /**
  * Planner rule that copies a {@link org.apache.calcite.rel.core.Sort} past a
  * {@link org.apache.calcite.rel.core.Join} without its limit and offset.
+ * Original {@link org.apache.calcite.rel.core.Sort} is preserved but
+ * can be removed by {@link org.apache.calcite.rel.rules.SortRemoveRule} if redundant.
+ *
+ * {@link org.apache.calcite.rel.rules.SortJoinCopyRule} allows a {@link org.apache.calcite.rel.core.Sort} to be
+ * incorporated in an index scan, it makes the use of operators requiring
+ * sorted inputs easier and it can reduce the size of the relations being sorted.
  */
 public class SortJoinCopyRule extends RelOptRule {
 
